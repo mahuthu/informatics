3cc03b41dfd04eb22e1930cc0dee9d46aa9005e4
mahuthu <mahuthup@gmail.com>
2024-05-03 09:32:47 +0300

    first commit

---
+++ api/user/views.py
@@ -0,0 +1,24 @@
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+
+
+def login(request):
+    return render(request, 'user/login.html')
+
+def home(request):
+    return render( request, "user/home.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+
